5e9d69d0990685407e5254e952ce4f1af9b36239
bobbyxii <bobanthony45@gmail.com>
2022-03-19 07:09:25 +0100

    Add Hashing

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 
@@ -25,7 +24,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]  });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,7 +42,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function (err) {
@@ -60,9 +58,9 @@ app.post("/register", function (req, res) {
 app.post("/login", function (req, res) {
   User.findOne({email: req.body.username}, function (err, foundUser) {
     if (foundUser){
-      if (foundUser.password === req.body.password) {
+      if (foundUser.password === md5(req.body.password)) {
         res.render("secrets");
-      } else {
+      } else {      
         res.send("Incorrect password");
       }
     } else {
